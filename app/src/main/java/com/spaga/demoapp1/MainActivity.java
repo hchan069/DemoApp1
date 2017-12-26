e1418861e3ea0129f60538cc35cdb966ba2f8b1d
spaga <spagabtti@gmail.com>
2017-12-26 13:03:44 -0800

    edit

--- app/src/main/java/com/spaga/demoapp1/MainActivity.java
+++ app/src/main/java/com/spaga/demoapp1/MainActivity.java
@@ -5,12 +5,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
         EditText myTextField = findViewById(R.id.myTextField);
         Log.i("Info", myTextField.getText().toString());
+        Toast.makeText(this, myTextField.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void changeImage(View view) {
+        ImageView image = findViewById(R.id.image1);
+        image.setImageResource(R.drawable.dog1);
+
+        Log.i("test", "Button clicked");
+
     }
 
     @Override
